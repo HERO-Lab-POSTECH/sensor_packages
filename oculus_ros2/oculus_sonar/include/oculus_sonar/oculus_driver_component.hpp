06bf9f75379aa10f8614d34896040a875230b783
luckkim123 <luckkim123@gmail.com>
2026-05-04 13:27:58 +0900

    refactor(oculus_sonar): extract OculusDriverPublishers (B-1 step 2/3)
    4 sensor + 9 param echo publisher와 publishParameters, sonarToImage,
    templated publishPing<PingT>를 OculusDriverPublishers로 이전.
    OculusDriver는 단순히 publishers_->publishPing(ping)으로 위임.
    
    structural snapshot regression PASS.

--- oculus_ros2/oculus_sonar/include/oculus_sonar/oculus_driver_component.hpp
+++ oculus_ros2/oculus_sonar/include/oculus_sonar/oculus_driver_component.hpp
@@ -31,6 +31,7 @@
 #include "oculus_sonar/publishing_data_rx.h"
 #include "oculus_sonar/ping_to_sonar_image.hpp"
 #include "oculus_sonar/oculus_driver_config.hpp"
+#include "oculus_sonar/oculus_driver_publishers.hpp"
 #include "marine_acoustic_msgs/msg/sonar_image.hpp"
 #include "oculus_sonar_msgs/msg/oculus_metadata.hpp"
 #include "apl_msgs/msg/raw_data.hpp"
@@ -61,20 +62,7 @@ class OculusDriver : public rclcpp::Node {
    */
   template <typename Ping_t>
   void pingCallback(const Ping_t &ping) {
-    auto sonar_msg = pingToSonarImage(ping);
-    sonar_msg.header.stamp = this->get_clock()->now();
-    sonar_msg.header.frame_id = config_->frame_id();
-    imaging_sonar_pub_->publish(sonar_msg);
-    auto image_msg = sonarToImage(sonar_msg);
-    image_pub_.publish(image_msg);
-
-    oculus_sonar_msgs::msg::OculusMetadata meta;
-    meta.header = sonar_msg.header;
-    for (unsigned int i = 0; i < ping.gains().size(); i++) {
-      meta.tvg.push_back(ping.gains().at(i));
-    }
-    oculus_meta_pub_->publish(meta);
-    publishParameters();
+    publishers_->publishPing(ping);
   }
 
  private:
@@ -83,35 +71,12 @@ class OculusDriver : public rclcpp::Node {
    */
   void init();
 
-  /**
-   * @brief Convert SonarImage to standard Image for rviz2 visualization.
-   * @param sonar_msg SonarImage message.
-   * @return sensor_msgs::msg::Image for visualization.
-   */
-  sensor_msgs::msg::Image sonarToImage(const marine_acoustic_msgs::msg::SonarImage& sonar_msg);
-
   liboculus::IoServiceThread io_srv_;
   std::unique_ptr<PublishingDataRx> data_rx_;
   liboculus::StatusRx status_rx_;
 
-  rclcpp::Publisher<marine_acoustic_msgs::msg::SonarImage>::SharedPtr imaging_sonar_pub_;
-  rclcpp::Publisher<oculus_sonar_msgs::msg::OculusMetadata>::SharedPtr oculus_meta_pub_;
-  rclcpp::Publisher<apl_msgs::msg::RawData>::SharedPtr raw_data_pub_;
-  image_transport::Publisher image_pub_;
-
-  // Parameter publishers for recording
-  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr ping_rate_pub_;
-  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr freq_mode_pub_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr data_size_pub_;
-  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr range_pub_;
-  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr gain_pub_;
-  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr gamma_pub_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr ip_address_pub_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr frame_id_pub_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr sonar_model_pub_;
-  void publishParameters();
-
   std::unique_ptr<OculusDriverConfig> config_;
+  std::unique_ptr<OculusDriverPublishers> publishers_;
 };
 
 }  // namespace oculus_sonar
--- oculus_ros2/oculus_sonar/src/oculus_driver_component.cpp
+++ oculus_ros2/oculus_sonar/src/oculus_driver_component.cpp
@@ -13,6 +13,7 @@
 #include "liboculus/SonarConfiguration.h"
 #include "oculus_sonar/oculus_driver_component.hpp"
 #include "oculus_sonar/oculus_driver_config.hpp"
+#include "oculus_sonar/oculus_driver_publishers.hpp"
 #include "oculus_sonar/publishing_data_rx.h"
 #include "oculus_sonar/sonar_config.hpp"
 
@@ -43,32 +44,10 @@ void OculusDriver::init() {
                this->get_namespace());
 
   std::string topic_prefix = "/sensor/sonar/oculus/" + config_->sonar_model();
+  publishers_ = std::make_unique<OculusDriverPublishers>(this, topic_prefix, config_->frame_id());
+  publishers_->initialize();
 
-  // Create publishers
-  // Use BEST_EFFORT QoS for all sensor data publishers
-  auto sensor_qos = rclcpp::SensorDataQoS();
-
-  imaging_sonar_pub_ = this->create_publisher<marine_acoustic_msgs::msg::SonarImage>(
-    topic_prefix + "/sonar", sensor_qos);  // SonarImage 메시지
-  oculus_meta_pub_ = this->create_publisher<oculus_sonar_msgs::msg::OculusMetadata>(
-    topic_prefix + "/metadata", sensor_qos);
-  raw_data_pub_ = this->create_publisher<apl_msgs::msg::RawData>(topic_prefix + "/raw_data", sensor_qos);
-  image_pub_ = image_transport::create_publisher(this, topic_prefix + "/image", rmw_qos_profile_sensor_data);  // polar 이미지 (raw + compressed 자동 생성)
-
-  // Create parameter publishers for recording
-  ping_rate_pub_ = this->create_publisher<std_msgs::msg::Int32>(topic_prefix + "/param/ping_rate", sensor_qos);
-  freq_mode_pub_ = this->create_publisher<std_msgs::msg::Int32>(topic_prefix + "/param/freq_mode", sensor_qos);
-  data_size_pub_ = this->create_publisher<std_msgs::msg::String>(topic_prefix + "/param/data_size", sensor_qos);
-  range_pub_ = this->create_publisher<std_msgs::msg::Float32>(topic_prefix + "/param/range", sensor_qos);
-  gain_pub_ = this->create_publisher<std_msgs::msg::Int32>(topic_prefix + "/param/gain", sensor_qos);
-  gamma_pub_ = this->create_publisher<std_msgs::msg::Int32>(topic_prefix + "/param/gamma", sensor_qos);
-  ip_address_pub_ = this->create_publisher<std_msgs::msg::String>(topic_prefix + "/param/ip_address", sensor_qos);
-  frame_id_pub_ = this->create_publisher<std_msgs::msg::String>(topic_prefix + "/param/frame_id", sensor_qos);
-  sonar_model_pub_ = this->create_publisher<std_msgs::msg::String>(topic_prefix + "/param/sonar_model", sensor_qos);
-
-  RCLCPP_INFO(this->get_logger(), "Publishing data with frame = %s", config_->frame_id().c_str());
-
-  data_rx_->setRawPublisher(raw_data_pub_);
+  data_rx_->setRawPublisher(publishers_->raw_data_publisher());
 
   data_rx_->setCallback<SimplePingResultV1>(std::bind(&OculusDriver::pingCallback<SimplePingResultV1>,
                                             this, std::placeholders::_1));
@@ -135,72 +114,6 @@ void OculusDriver::init() {
   }
 
   io_srv_.start();
-}
-
-sensor_msgs::msg::Image OculusDriver::sonarToImage(
-    const marine_acoustic_msgs::msg::SonarImage& sonar_msg) {
-  sensor_msgs::msg::Image image_msg;
-  
-  image_msg.header = sonar_msg.header;
-  
-  // ranges와 azimuth_angles의 크기로 이미지 크기 계산
-  image_msg.height = sonar_msg.ranges.size();
-  image_msg.width = sonar_msg.azimuth_angles.size();
-  
-  // data_size에 따른 인코딩 설정
-  if (sonar_msg.data_size == 1) {
-    image_msg.encoding = "mono8";
-  } else if (sonar_msg.data_size == 2) {
-    image_msg.encoding = "mono16";  // ROS2에서는 "mono16" 사용
-  } else if (sonar_msg.data_size == 4) {
-    image_msg.encoding = "32FC1";
-  }
-  
-  image_msg.is_bigendian = sonar_msg.is_bigendian;
-  image_msg.step = image_msg.width * sonar_msg.data_size;
-  
-  // intensities 배열을 image data로 복사
-  image_msg.data = sonar_msg.intensities;
-  
-  return image_msg;
-}
-void OculusDriver::publishParameters() {
-  // Publish current parameters
-  std_msgs::msg::Int32 ping_rate_msg;
-  ping_rate_msg.data = this->get_parameter("ping_rate").as_int();
-  ping_rate_pub_->publish(ping_rate_msg);
-  
-  std_msgs::msg::Int32 freq_mode_msg;
-  freq_mode_msg.data = this->get_parameter("freq_mode").as_int();
-  freq_mode_pub_->publish(freq_mode_msg);
-  
-  std_msgs::msg::String data_size_msg;
-  data_size_msg.data = this->get_parameter("data_size").as_string();
-  data_size_pub_->publish(data_size_msg);
-  
-  std_msgs::msg::Float32 range_msg;
-  range_msg.data = static_cast<float>(this->get_parameter("range").as_double());
-  range_pub_->publish(range_msg);
-  
-  std_msgs::msg::Int32 gain_msg;
-  gain_msg.data = this->get_parameter("gain").as_int();
-  gain_pub_->publish(gain_msg);
-  
-  std_msgs::msg::Int32 gamma_msg;
-  gamma_msg.data = this->get_parameter("gamma").as_int();
-  gamma_pub_->publish(gamma_msg);
-  
-  std_msgs::msg::String ip_msg;
-  ip_msg.data = this->get_parameter("ip_address").as_string();
-  ip_address_pub_->publish(ip_msg);
-  
-  std_msgs::msg::String frame_id_msg;
-  frame_id_msg.data = this->get_parameter("frame_id").as_string();
-  frame_id_pub_->publish(frame_id_msg);
-
-  std_msgs::msg::String sonar_model_msg;
-  sonar_model_msg.data = this->get_parameter("sonar_model").as_string();
-  sonar_model_pub_->publish(sonar_model_msg);
 }
 
 }  // namespace oculus_sonar
